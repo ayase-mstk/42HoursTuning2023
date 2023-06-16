926e582b0a70c4ce6e605f7be234c133c9aa3fec
msano42 <test@gmail.com>
2023-06-16 15:04:12 +0000

    more index updates

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -3,3 +3,7 @@ CREATE INDEX user_idx1 ON user (office_id);
 CREATE INDEX user_idx2 ON user (mail, password);
 
 CREATE INDEX department_role_member_idx0 ON department_role_member (role_id);
+
+CREATE INDEX match_group_member_idx0 ON match_group_member (user_id);
+
+CREATE INDEX dept_role_member_idx0 ON department_role_member (role_id);
