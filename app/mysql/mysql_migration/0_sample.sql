e580df93dbafe328fd10fc4b2e7701399a88dd6a
msano42 <test@gmail.com>
2023-06-16 14:57:14 +0000

    index update

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,5 +1,5 @@
 CREATE INDEX user_idx0 ON user (entry_date ASC, kana ASC);
 CREATE INDEX user_idx1 ON user (office_id);
-CREATE INDEX user_idx1 ON user (mail, password);
+CREATE INDEX user_idx2 ON user (mail, password);
 
 CREATE INDEX department_role_member_idx0 ON department_role_member (role_id);
