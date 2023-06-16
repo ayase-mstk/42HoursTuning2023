8b5f4e1039aee99369b0090766c7f2bc6caf0cf4
msano42 <test@gmail.com>
2023-06-16 18:29:40 +0000

    edits

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,10 +1,11 @@
 CREATE INDEX user_idx0 ON user (entry_date ASC, kana ASC);
 CREATE INDEX user_idx1 ON user (office_id);
 CREATE INDEX user_idx2 ON user (mail, password);
+CREATE INDEX user_idx3 ON user (goal);
 
 CREATE INDEX dept_role_member_idx0 ON department_role_member (role_id);
 CREATE INDEX dept_role_member_idx1 ON department_role_member (user_id);
 
 CREATE INDEX match_group_member_idx0 ON match_group_member (user_id);
 
-CREATE INDEX goal_idx0 ON user (goal);
+CREATE INDEX match_groupidx0 ON match_group (match_group_name);
