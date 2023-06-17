1282937ceed98786312473c3f927f17321d9db7c
msano42 <test@gmail.com>
2023-06-17 19:53:16 +0000

    more indexes!

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -8,10 +8,19 @@ CREATE INDEX dept_role_member_idx0 ON department_role_member (role_id);
 CREATE INDEX dept_role_member_idx1 ON department_role_member (user_id);
 
 CREATE INDEX match_group_member_idx0 ON match_group_member (user_id);
+CREATE INDEX match_group_member_idx1 ON match_group_member (match_group_id);
+    -- "SELECT user_id FROM match_group_member WHERE match_group_id IN (?)"
 
-CREATE INDEX match_groupidx0 ON match_group (match_group_name);
+CREATE INDEX match_group_idx0 ON match_group (match_group_name);
+CREATE INDEX match_group_idx1 ON match_group (match_group_id);
+    -- "SELECT match_group_id, match_group_name, description, status, created_by, created_at FROM match_group WHERE match_group_id = ?";
+CREATE INDEX match_group_member_idx2 ON match_group_member (user_id);
+    -- "SELECT match_group_id FROM match_group_member WHERE user_id = ?",
 
-CREATE INDEX sessionidx0 ON `session` (linked_user_id);
+CREATE INDEX session_idx0 ON `session` (linked_user_id);
+CREATE INDEX session_idx1 ON `session` (session_id);
+-- "SELECT * FROM session WHERE session_id = ?" <== unecessary??
+
 
 create table user_count (
     cnt int not null
@@ -19,3 +28,30 @@ create table user_count (
 insert into user_count (cnt) values ((select count(user_id) from user));
 
 create index skill_idx ON skill (skill_name);
+
+CREATE INDEX file_idx0 ON `file` (file_id);
+    -- "SELECT file_name, path FROM file WHERE file_id = ?" 
+
+CREATE INDEX office_idx0 ON office (office_id);
+-- `SELECT office_name FROM office WHERE office_id = ?`,
+
+
+
+-- "INSERT INTO match_group (match_group_id, match_group_name, description, status, created_by, created_at) VALUES (?, ?, ?, ?, ?, ?)",
+-- "INSERT INTO match_group_member (match_group_id, user_id) VALUES (?, ?)"
+-- "INSERT INTO session (session_id, linked_user_id, created_at) VALUES (?, ?, ?)",
+-- "DELETE FROM session WHERE linked_user_id = ?" <== index already exists anyways
+-- "SELECT user_id FROM user WHERE mail = ? AND password = ?",
+-- `SELECT user_id, user_name, office_id, user_icon_id FROM user ORDER BY entry_date ASC, kana ASC LIMIT ? OFFSET ?`
+-- "SELECT user_id, user_name, office_id, user_icon_id FROM user WHERE user_id = ?",
+-- "SELECT user_id, user_name, kana, entry_date, office_id, user_icon_id FROM user WHERE user_id = ?",
+-- `SELECT department_id FROM department WHERE department_name LIKE ? AND active = true`
+-- `SELECT user_id FROM department_role_member WHERE department_id IN (?) AND belong = true`
+--  `SELECT role_id FROM role WHERE role_name LIKE ? AND active = true`,
+-- `SELECT user_id FROM department_role_member WHERE role_id IN (?) AND belong = true`
+-- `SELECT office_id FROM office WHERE office_name LIKE ?`,
+-- `SELECT user_id FROM user WHERE office_id IN (?)`
+-- `SELECT skill_id FROM skill WHERE skill_name LIKE ?`
+-- `SELECT user_id FROM skill_member WHERE skill_id IN (?)`
+-- `SELECT department_name FROM department WHERE department_id = (SELECT department_id FROM department_role_member WHERE user_id = ? AND belong = true)`,
+-- `SELECT skill_name FROM skill WHERE skill_id IN (SELECT skill_id FROM skill_member WHERE user_id = ?)`,
--- app/node/src/routes/match-groups/repository.ts
+++ app/node/src/routes/match-groups/repository.ts
@@ -8,7 +8,9 @@ export const hasSkillNameRecord = async (
   skillName: string
 ): Promise<boolean> => {
   const [rows] = await pool.query<RowDataPacket[]>(
-    "SELECT * FROM skill WHERE EXISTS (SELECT * FROM skill WHERE skill_name = ?)",
+    // "SELECT * FROM skill WHERE EXISTS (SELECT * FROM skill WHERE skill_name = ?)",
+    // [skillName]
+    "SELECT * FROM skill WHERE skill_name = ? LIMIT 1",
     [skillName]
   );
   return rows.length > 0;
